b3bc2f30156eb53a8638ef79b94b5c5ca9f6c2bd
legit92 <nitindhake3@gmail.com>
2025-02-22 10:22:59 +0530

    completed ass

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,22 @@
-// write the component code here
+import React from "react";
+
+const Usercard = () => {
+    const user = {
+        profilePhoto: "https://placehold.co/150",
+        name: "Nitin",
+        email: "nitin@kalvium.community",
+        phoneNumber: "+91-1234567890",
+        address: "MIT ADTU, LONI GAON"
+    }
+    return (
+        <div>
+            <img src={user.profilePhoto} alt="UserImage"/>
+            <h2>{user.name}</h2>
+            <p><strong>Email:</strong> {user.email}</p>
+            <p><strong>Phone Number:</strong> {user.phoneNumber}</p>
+            <p><strong>Address:</strong> {user.address}</p>
+        </div>
+    );    
+}
+
+export default Usercard;
